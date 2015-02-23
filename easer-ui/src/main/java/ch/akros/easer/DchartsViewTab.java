ca9ecf45e8e07c2b01de9b279c489397e3a6c6c7
Gabor Lukovics <gabor.lukovics@akros.ch>
2015-02-23 14:05:22 +0100

    charts verbessert

--- easer-ui/src/main/java/ch/akros/easer/DchartsViewTab.java
+++ easer-ui/src/main/java/ch/akros/easer/DchartsViewTab.java
@@ -7,14 +7,16 @@ import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.VerticalLayout;
 import org.dussan.vaadin.dcharts.DCharts;
 import org.dussan.vaadin.dcharts.base.elements.XYaxis;
+import org.dussan.vaadin.dcharts.base.elements.XYseries;
 import org.dussan.vaadin.dcharts.data.DataSeries;
 import org.dussan.vaadin.dcharts.data.Ticks;
+import org.dussan.vaadin.dcharts.metadata.TooltipAxes;
+import org.dussan.vaadin.dcharts.metadata.XYaxes;
+import org.dussan.vaadin.dcharts.metadata.locations.TooltipLocations;
 import org.dussan.vaadin.dcharts.metadata.renderers.AxisRenderers;
 import org.dussan.vaadin.dcharts.metadata.renderers.SeriesRenderers;
-import org.dussan.vaadin.dcharts.options.Axes;
-import org.dussan.vaadin.dcharts.options.Highlighter;
-import org.dussan.vaadin.dcharts.options.Options;
-import org.dussan.vaadin.dcharts.options.SeriesDefaults;
+import org.dussan.vaadin.dcharts.options.*;
+import org.dussan.vaadin.dcharts.renderers.tick.AxisTickRenderer;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -44,7 +46,7 @@ public class DchartsViewTab extends CustomComponent implements EaserTab {
                 createNamedQuery(TankFuellung.FIND_ALL, TankFuellung.class);
 
         // we create for every year a bar chart, so we filter all possible years
-        List<Integer> years = namedQuery.getResultList().stream().map(e -> e.getDatum().getYear()).distinct().collect(Collectors.toList());
+        List<Integer> years = namedQuery.getResultList().stream().map(e -> e.getDatum().getYear()).distinct().sorted().collect(Collectors.toList());
 
         // iterate over all years
         for (int year : years) {
@@ -52,18 +54,18 @@ public class DchartsViewTab extends CustomComponent implements EaserTab {
             Map<Month, List<TankFuellung>> yearMap = new HashMap<>();
             List<TankFuellung> tankfuellungenInYear = namedQuery.getResultList().stream().filter(e -> e.getDatum().getYear() == year).collect(Collectors.toList());
 
-            List<TankFuellung> tankfulInJanuary = namedQuery.getResultList().stream().filter(e -> e.getDatum().getMonth() == Month.JANUARY).collect(Collectors.toList());
-            List<TankFuellung> tankfulInFebruary = namedQuery.getResultList().stream().filter(e -> e.getDatum().getMonth() == Month.FEBRUARY).collect(Collectors.toList());
-            List<TankFuellung> tankfulInMarch = namedQuery.getResultList().stream().filter(e -> e.getDatum().getMonth() == Month.MARCH).collect(Collectors.toList());
-            List<TankFuellung> tankfulInApril = namedQuery.getResultList().stream().filter(e -> e.getDatum().getMonth() == Month.APRIL).collect(Collectors.toList());
-            List<TankFuellung> tankfulInMay = namedQuery.getResultList().stream().filter(e -> e.getDatum().getMonth() == Month.MAY).collect(Collectors.toList());
-            List<TankFuellung> tankfulInJune = namedQuery.getResultList().stream().filter(e -> e.getDatum().getMonth() == Month.JUNE).collect(Collectors.toList());
-            List<TankFuellung> tankfulInJuly = namedQuery.getResultList().stream().filter(e -> e.getDatum().getMonth() == Month.JULY).collect(Collectors.toList());
-            List<TankFuellung> tankfulInAugust = namedQuery.getResultList().stream().filter(e -> e.getDatum().getMonth() == Month.AUGUST).collect(Collectors.toList());
-            List<TankFuellung> tankfulInSeptember = namedQuery.getResultList().stream().filter(e -> e.getDatum().getMonth() == Month.SEPTEMBER).collect(Collectors.toList());
-            List<TankFuellung> tankfulInOctober = namedQuery.getResultList().stream().filter(e -> e.getDatum().getMonth() == Month.OCTOBER).collect(Collectors.toList());
-            List<TankFuellung> tankfulInNovmeber = namedQuery.getResultList().stream().filter(e -> e.getDatum().getMonth() == Month.NOVEMBER).collect(Collectors.toList());
-            List<TankFuellung> tankfulInDecember = namedQuery.getResultList().stream().filter(e -> e.getDatum().getMonth() == Month.DECEMBER).collect(Collectors.toList());
+            List<TankFuellung> tankfulInJanuary = tankfuellungenInYear.stream().filter(e -> e.getDatum().getMonth() == Month.JANUARY).collect(Collectors.toList());
+            List<TankFuellung> tankfulInFebruary = tankfuellungenInYear.stream().filter(e -> e.getDatum().getMonth() == Month.FEBRUARY).collect(Collectors.toList());
+            List<TankFuellung> tankfulInMarch = tankfuellungenInYear.stream().filter(e -> e.getDatum().getMonth() == Month.MARCH).collect(Collectors.toList());
+            List<TankFuellung> tankfulInApril = tankfuellungenInYear.stream().filter(e -> e.getDatum().getMonth() == Month.APRIL).collect(Collectors.toList());
+            List<TankFuellung> tankfulInMay = tankfuellungenInYear.stream().filter(e -> e.getDatum().getMonth() == Month.MAY).collect(Collectors.toList());
+            List<TankFuellung> tankfulInJune = tankfuellungenInYear.stream().filter(e -> e.getDatum().getMonth() == Month.JUNE).collect(Collectors.toList());
+            List<TankFuellung> tankfulInJuly = tankfuellungenInYear.stream().filter(e -> e.getDatum().getMonth() == Month.JULY).collect(Collectors.toList());
+            List<TankFuellung> tankfulInAugust = tankfuellungenInYear.stream().filter(e -> e.getDatum().getMonth() == Month.AUGUST).collect(Collectors.toList());
+            List<TankFuellung> tankfulInSeptember = tankfuellungenInYear.stream().filter(e -> e.getDatum().getMonth() == Month.SEPTEMBER).collect(Collectors.toList());
+            List<TankFuellung> tankfulInOctober = tankfuellungenInYear.stream().filter(e -> e.getDatum().getMonth() == Month.OCTOBER).collect(Collectors.toList());
+            List<TankFuellung> tankfulInNovmeber = tankfuellungenInYear.stream().filter(e -> e.getDatum().getMonth() == Month.NOVEMBER).collect(Collectors.toList());
+            List<TankFuellung> tankfulInDecember = tankfuellungenInYear.stream().filter(e -> e.getDatum().getMonth() == Month.DECEMBER).collect(Collectors.toList());
 
             yearMap.put(Month.JANUARY, tankfulInJanuary);
             yearMap.put(Month.FEBRUARY, tankfulInFebruary);
@@ -79,11 +81,14 @@ public class DchartsViewTab extends CustomComponent implements EaserTab {
             yearMap.put(Month.DECEMBER, tankfulInDecember);
 
             Month monthWithMostTankfuellungen = getMonthWithMostTankfuellungen(yearMap);
-            int sizeOfThisMonth = yearMap.get(monthWithMostTankfuellungen).size();
+            int numberOfTankfuellungen = yearMap.get(monthWithMostTankfuellungen).size();
 
             DataSeries dataSeries = new DataSeries();
-            for (int i = 0; i < sizeOfThisMonth; i++) {
+            Series series = new Series();
+            // the numberOfTankfuellungen is the number of iterations(one iteration is one dataserie)
+            for (int i = 0; i < numberOfTankfuellungen; i++) {
                 List<Double> data = new ArrayList<>(Arrays.asList(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0));
+                // iterate over every month
                 for (Map.Entry<Month, List<TankFuellung>> entry : yearMap.entrySet()) {
                     if (entry.getValue().size() > i) {
                         switch (entry.getKey()) {
@@ -124,25 +129,48 @@ public class DchartsViewTab extends CustomComponent implements EaserTab {
                                 data.set(11, entry.getValue().get(i).getMenge().doubleValue());
                                 break;
                         }
+
                     }
                 }
+
+                series.addSeries(new XYseries().setLabel("Tankfüllung"));
                 dataSeries.add(data.toArray());
+
             }
 
             SeriesDefaults seriesDefaults = new SeriesDefaults().setRenderer(SeriesRenderers.BAR);
+
             Axes axes = new Axes().addAxis(new XYaxis()
                     .setRenderer(AxisRenderers.CATEGORY)
-                    .setTicks(new Ticks().add("Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember")));
-            Highlighter highlighter = new Highlighter().setShow(false);
+                    .setTicks(new Ticks().add("Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember")))
+                    .addAxis(
+                            new XYaxis(XYaxes.Y)
+                                    .setTickOptions(
+                                            new AxisTickRenderer()
+                                                    .setFormatString("%d l")));
 
+            Highlighter highlighter = new Highlighter()
+                    .setShow(true)
+                    .setShowTooltip(true)
+                    .setTooltipAlwaysVisible(true)
+                    .setKeepTooltipInsideChart(true)
+                    .setTooltipLocation(TooltipLocations.NORTH)
+                    .setTooltipAxes(TooltipAxes.XY_BAR);
+
+            String titleAsString = "Tankfüllungen im Jahr " + year;
+            Title title = new Title(titleAsString);
             Options options = new Options()
                     .setSeriesDefaults(seriesDefaults)
+                    .setTitle(title)
+                    .setSeries(series)
                     .setAxes(axes)
                     .setHighlighter(highlighter);
 
+
             DCharts chart = new DCharts();
             chart.setSizeFull();
-            chart.setDescription(String.valueOf(year));
+            chart.setMarginRight(10);
+            chart.setDescription(titleAsString);
             chart.setDataSeries(dataSeries)
                     .setOptions(options)
                     .show();
